f6a6341b11673c899cbf9181f0f06198def0b396
shakirr1176 <shakir@appolay.com>
2024-02-13 17:17:44 +0600

    upate html

--- js/main.js
+++ js/main.js
@@ -31,3 +31,58 @@ let circleIcon = document.querySelectorAll('.circle-icon')
 let circleIconActive = [...circleIcon].filter(el=>el.classList.contains('active'))[0]
 
 activeBtn(circleIcon,circleIconActive)
+
+
+let parametrosTab = document.querySelectorAll('.parametros-tab')
+let parametrosActive = [...parametrosTab].filter(el=>el.classList.contains('active'))[0]
+
+activeBtn(parametrosTab,parametrosActive)
+
+
+let playPauseBtn = document.querySelector('.play-pause-btn')
+
+if(playPauseBtn){
+    // window.addEventListener('load',()=>{
+    // })
+    
+    playPauseFunc(playPauseBtn)
+    playPauseBtn?.addEventListener('click',()=>{
+
+        playPauseBtn.classList.toggle('play')
+        playPauseBtn.classList.toggle('pause')
+
+        playPauseFunc(playPauseBtn)
+    })
+    
+    function playPauseFunc(btn){
+
+        let svg = btn.querySelector('.icon')
+        let span = btn.querySelector('.text')
+
+        if(btn?.classList.contains('play')){
+            if(svg){
+                svg.querySelector('path').setAttribute('d','M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z')
+            }
+            if(span){
+                span.innerText = 'pause'
+                span?.classList.add('fade')
+            }
+
+            setTimeout(()=>{
+                span.classList.remove('fade')
+            },150)
+        }else{
+            if(svg){
+                svg.querySelector('path').setAttribute('d','M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z')
+            }
+            if(span){
+                span.innerText = 'play'
+                span?.classList.add('fade')
+            }
+
+            setTimeout(()=>{
+                span.classList.remove('fade')
+            },150)
+        }
+    }
+}
